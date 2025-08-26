db31cd0b2c22c8e2a4c0cba36e79241ab00f0dbb
grisonb <79510016+grisonb@users.noreply.github.com>
2025-08-26 13:29:46 +0200

    v5.1.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'test-communes-app-cache-v300'; 
-const DATA_CACHE_NAME = 'test-communes-data-cache-v300';
-const TILE_CACHE_NAME = 'test-communes-tile-cache-v300';
+const APP_CACHE_NAME = 'test-communes-app-cache-v510'; 
+const DATA_CACHE_NAME = 'test-communes-data-cache-v510';
+const TILE_CACHE_NAME = 'test-communes-tile-cache-v510';
 
 const APP_SHELL_URLS = [
     './',
